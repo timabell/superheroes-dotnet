905ad074582b65c818bb2ea72598d83f760a365a
Tim Abell <tim@timwise.co.uk>
2023-04-18 12:54:49 +0100

    Failing test for hero winning when matched

--- Superheroes.Tests/BattleTests.cs
+++ Superheroes.Tests/BattleTests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.TestHost;
 using System.Threading.Tasks;
 using FluentAssertions;
 using System.Net;
+using System.Net.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,45 +14,69 @@ namespace Superheroes.Tests
     public class BattleTests
     {
         [Fact]
-        public async Task CanGetHeros()
+        public async Task Battle_Hero_Wins()
         {
             var charactersProvider = new FakeCharactersProvider();
+            var client = Setup(charactersProvider);
+            SetupDataFixture(charactersProvider, heroName: "Batman", heroScore: 8.3, villainName: "Joker", villainScore: 8.2);
 
-            var startup = new WebHostBuilder()
-                            .UseStartup<Startup>()
-                            .ConfigureServices(x => 
-                            {
-                                x.AddSingleton<ICharactersProvider>(charactersProvider);
-                            });
-            var testServer = new TestServer(startup);
-            var client = testServer.CreateClient();
+            var response = await RunBattle(client);
 
-            charactersProvider.FakeResponse(new CharactersResponse
-            {
-                Items = new []
-                {
-                    new CharacterResponse
-                    {
-                        Name = "Batman",
-                        Score = 8.3,
-                        Type = "hero"
-                    },
-                    new CharacterResponse
-                    {
-                        Name = "Joker",
-                        Score = 8.2,
-                        Type = "villain"
-                    }
-                }
-            });
+            response.Value<string>("name").Should().Be("Batman");
+        }
 
+        [Fact]
+        public async Task Battle_Hero_Wins_When_Matches()
+        {
+            var charactersProvider = new FakeCharactersProvider();
+            var client = Setup(charactersProvider);
+            SetupDataFixture(charactersProvider, heroName: "Batman", heroScore: 8.2, villainName: "Joker", villainScore: 8.2);
+
+            var response = await RunBattle(client);
+
+            response.Value<string>("name").Should().Be("Batman");
+        }
+
+        private static async Task<JObject> RunBattle(HttpClient client)
+        {
             var response = await client.GetAsync("battle?hero=Batman&villain=Joker");
             response.StatusCode.Should().Be(HttpStatusCode.OK);
 
             var responseJson = await response.Content.ReadAsStringAsync();
             var responseObject = JsonConvert.DeserializeObject<JObject>(responseJson);
+            return responseObject;
+        }
 
-            responseObject.Value<string>("name").Should().Be("Batman");
+        private static void SetupDataFixture(FakeCharactersProvider charactersProvider, string heroName, double heroScore, string villainName, double villainScore)
+        {
+            charactersProvider.FakeResponse(new CharactersResponse
+            {
+                Items = new[]
+                {
+                    new CharacterResponse
+                    {
+                        Name = heroName,
+                        Score = heroScore,
+                        Type = "hero"
+                    },
+                    new CharacterResponse
+                    {
+                        Name = villainName,
+                        Score = villainScore,
+                        Type = "villain"
+                    }
+                }
+            });
+        }
+
+        private static HttpClient Setup(FakeCharactersProvider charactersProvider)
+        {
+            var startup = new WebHostBuilder()
+                .UseStartup<Startup>()
+                .ConfigureServices(x => { x.AddSingleton<ICharactersProvider>(charactersProvider); });
+            var testServer = new TestServer(startup);
+            var client = testServer.CreateClient();
+            return client;
         }
     }
 }
--- Superheroes/Controllers/BattleController.cs
+++ Superheroes/Controllers/BattleController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,8 +8,6 @@ namespace Superheroes.Controllers
     public class BattleController : Controller
     {
         private readonly ICharactersProvider _charactersProvider;
-        private static CharacterResponse _character1;
-        private static CharacterResponse _character2;
 
         public BattleController(ICharactersProvider charactersProvider)
         {
@@ -17,26 +16,19 @@ namespace Superheroes.Controllers
 
         public async Task<IActionResult> Get(string hero, string villain)
         {
-            var characters = await _charactersProvider.GetCharacters();
-            
-            foreach(var character in characters.Items)
+            CharactersResponse availableCharacters = await _charactersProvider.GetCharacters();
+
+            CharacterResponse heroCharactar = null;
+            CharacterResponse villainCharacter = null;
+            heroCharactar = availableCharacters.Items.SingleOrDefault(c => c.Name == hero);
+            villainCharacter = availableCharacters.Items.SingleOrDefault(c => c.Name == villain);
+
+            if(heroCharactar.Score > villainCharacter.Score)
             {
-                if(character.Name == hero)
-                {
-                    _character1 = character;
-                }
-                if(character.Name == villain)
-                {
-                    _character2 = character;
-                }
+                return Ok(heroCharactar);
             }
 
-            if(_character1.Score > _character2.Score)
-            {
-                return Ok(_character1);
-            }
-
-            return Ok(_character2);
+            return Ok(villainCharacter);
         }
     }
 }
